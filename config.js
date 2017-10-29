d7768b45ef54e401b83edb7faccbb17a515866a3
Carmen Liu <carmen.liujia@gmail.com>
2017-10-29 17:39:46 +1300

    provide webhook for facebook messager

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  FB_VERIFY_TOKEN: 'iamninjasbotscalett'
+};
--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 'user strict'
 
+const config = require('./config');
 const express = require('express');
+const bodyPraser = require('body-parser);
 
 var app = express();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+//facebook webhook
+app.get('webhook', (req, res) => {
+  if(req.query['hub.mode'] === "subscribe" && req.query["hub.verify_token"] === config.FB_VERIFY_TOKEN) {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation, Make sure the validation token match");
+    res.sendStatus(403);
+  }
+});
+
+
 app.listen(port, ()=> {
   console.log(`Start up at port ${port}`);
 });
