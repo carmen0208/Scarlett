13358921f3b59752a9f51098c02d079fe917086a
Carmen Liu <carmen.liujia@gmail.com>
2017-10-29 20:07:29 +1300

    facebook messager post webhook

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  FB_VERIFY_TOKEN: 'iamninjasbotscalett'
+  FB_VERIFY_TOKEN: 'iamninjasbotscalett',
+  FB_PAGE_ACCESS_TOKEN: ''
+
 };
--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+
+const facebook_page_token = process.env.FB_PAGE_ACCESS_TOKEN || config.FB_PAGE_ACCESS_TOKEN;
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -23,7 +26,74 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+app.post('/webhook', (req, res) => {
+  // Parse the request body from the POST
+  let body = req.body;
+  console.log(JSON.stringify(body));
 
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      // Get the webhook event. entry.messaging is an array, but
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      } else {
+        console.log("Webhook received unknown messaging Events: ", webhook_event);
+      }
+    });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+  if(received_message.text) {
+    response = {
+      "text": `You send the message "${received_message.text}". Now send me an image!`
+    }
+  }
+  //Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/message",
+    "qs": { "access_token": facebook_page_token },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
 app.listen(port, ()=> {
   console.log(`Start up at port ${port}`);
 });
